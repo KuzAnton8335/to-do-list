a9f6789ce35aada42adf0149db5295c14a83a96b
KuzAnton8335 <ant2783@mail.com>
2023-11-28 22:04:17 +0300

    baks 3 bloks

--- modules/render.js
+++ modules/render.js
@@ -1,7 +1,8 @@
-import { h3, createForm, createTable } from "./createElements.js";
+// render
 
+import { createForm, createTableWrapper, title } from "./main.js";
 
 const appContainer = document.querySelector(".app-container");
 appContainer.classList.add("vh-100", "w-100", "d-flex", "align-items-center", "justify-content-center", "flex-column");
 
-appContainer.append(h3, createForm(), createTable(),);
+appContainer.append(title(), createForm(), createTableWrapper());
